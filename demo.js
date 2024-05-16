90f1411c6562cc18b903c31a77886cbfa3f7ab23
KovuriVaishnavi <22B81A6762@CVR.AC.IN>
2024-05-16 19:28:40 +0530

    update studentManagementSystem

--- demo.js
+++ demo.js
@@ -1,46 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    display()
+})
 
-document.addEventListener('DOMContentLoaded', function ()
-{
-    display();
-
-});
-
-function addStudent(event){
+function addStudent(event) {
     event.preventDefault();
-    let rollno=document.querySelector("#rollno").value;
-    let name=document.querySelector("#name").value;
-    let age=document.querySelector("#age").value;
-    let course=document.querySelector("#course").value
-    let id=Math.floor(Math.random()*1000).toString();
-    axios.post(`http://localhost:3000/students`,{id,rollno,name,age,course}).then(Response=>display());
-       
-    
+    let rollno = document.querySelector("#rollno").value;
+    let name = document.querySelector("#name").value;
+    let age = document.querySelector("#age").value;
+    let course = document.querySelector("#course").value;
+    let id = Math.floor(Math.random() * 1000).toString();
+
+    if (rollno === '' || name === '' || age === '' || course === '') {
+        alert("Please fill all the fields");
+    } else {
+        const tb = document.getElementById('t-body');
+        axios.get(`http://localhost:3000/students`)
+            .then(response => {
+                const data = response.data;
+                const existingStudent = data.find(student => student.rollno === rollno);
+                if (existingStudent) {
+                    alert("Entered student already exists");
+                } else {
+                    axios.post(`http://localhost:3000/students`, { id, rollno, name, age, course })
+                        .then(response => {
+                            display();
+                        })
+                        
+                }
+            })
+            
+    }
 }
+
 
 function display(){
     
-    axios.get(`http://localhost:3000/students`).then(response=>
-        displayDataInTable(response.data))
+    axios.get(`http://localhost:3000/students`)
+        .then(response => {
+            const data = response.data
+            displayDataInTable(data) 
+        })
 }
 
-function displayDataInTable(data) {
-    let tb=document.querySelector("#t-body");
-    tb.innerHTML = '';
-  
-    data.forEach(element => {
-        const row=tb.insertRow();
-        row.insertCell().textContent=element.rollno;
-        row.insertCell().textContent=element.name;
-        row.insertCell().textContent=element.age;
-        row.insertCell().textContent=element.course;
+
+function displayDataInTable(dataa) {
+    let tb = document.querySelector("#t-body");
+    tb.innerHTML = "";
+
+    dataa.forEach(element => {
+        const row = tb.insertRow();
+        row.insertCell().textContent = element.rollno;
+        row.insertCell().textContent = element.name;
+        row.insertCell().textContent = element.age;
+        row.insertCell().textContent = element.course;
         row.insertCell().innerHTML = `<button onclick="edit('${element.id}','${element.rollno}','${element.name}','${element.age}','${element.course}')" id="b"><i class="fa-solid fa-pen-to-square"></i></button>` + `<button onclick="deleteData(${element.id})" id="b"><i class="fa-solid fa-trash"></i></button>`;
-
-
     });
-  }
-  function resetForm(){
-    document.querySelector("#t-body").innerHTML="";
-  }
+}
+
+  
   function edit(i,r,n,a,c){
     let rollno=document.querySelector("#rollno").value;
     let name=document.querySelector("#name").value;
@@ -63,11 +80,45 @@ function displayDataInTable(data) {
     axios.delete(`http://localhost:3000/students/${id}`)
   }
   
-  function sortByField(field) {
+  function sortByFieldAsc(field) {
+    
     return function (a, b) {
-        if (a[field] > b[field]) {
+        let num1;
+    let num2;
+    if(field=='rollno'||field=='id'){
+        num1=Number(a[field])
+        num2=Number(b[field])
+    }
+    else{
+        num1=a[field]
+        num2=b[field]
+    }
+        if (num1 > num2) {
             return 1;
-        } else if (a[field] < b[field]) {
+        } else if (num1 <num2) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
+function sortByFieldDesc(field) {
+    
+    return function (a, b) {
+        let num1;
+    let num2;
+    if(field=='rollno'||field=='id'){
+        num1=Number(a[field])
+        num2=Number(b[field])
+    }
+    else{
+        num1=a[field]
+        num2=b[field]
+    }
+        if (num1 < num2) {
+            return 1;
+        } else if (num1 >num2) {
             return -1;
         }
         else {
@@ -82,7 +133,7 @@ function sortAsc(str) {
         .then(response => {
             tb.innerHTML = ""
             const data = response.data
-            data.sort(sortByField(str))
+            data.sort(sortByFieldAsc(str))
             data.forEach(element => {
                 const row=tb.insertRow();
                 row.insertCell().textContent=element.rollno;
@@ -94,3 +145,44 @@ function sortAsc(str) {
         
             })}
         )};
+
+        function sortDesc(str) {
+            const tb = document.getElementById('t-body');
+            axios(`http://localhost:3000/students`)
+                .then(response => {
+                    tb.innerHTML = ""
+                    const data = response.data
+                    data.sort(sortByFieldDesc(str))
+                    data.forEach(element => {
+                        const row=tb.insertRow();
+                        row.insertCell().textContent=element.rollno;
+                        row.insertCell().textContent=element.name;
+                        row.insertCell().textContent=element.age;
+                        row.insertCell().textContent=element.course;
+                        row.insertCell().innerHTML = `<button onclick="edit('${element.id}','${element.rollno}','${element.name}','${element.age}','${element.course}')" id="b"><i class="fa-solid fa-pen-to-square"></i></button>` + `<button onclick="deleteData(${element.id})" id="b"><i class="fa-solid fa-trash"></i></button>`;
+                
+                
+                    })}
+                )};
+
+   
+                async function searchDisplay()
+{   ele=[]
+    let b=false
+    let a=document.getElementById("rollno").value
+    await axios.get(`http://localhost:3000/students`)
+    .then(response=>{
+        const data=response.data
+        data.forEach(element=>{
+            if(element.rollno==a){
+                ele.push(element)
+                b=true
+            }
+        })
+       
+    })
+   console.log(ele)
+    displayDataInTable(ele)
+    return b
+}
+       
